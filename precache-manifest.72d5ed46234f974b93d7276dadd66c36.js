36115fd1ed236bc33444f606d93415099c02852c
ajit <ajitpal075@gmail.com>
2021-08-14 23:05:27 +0530

    Updates

---
+++ precache-manifest.72d5ed46234f974b93d7276dadd66c36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6744bb031dd87d47dfad4d3f5379fd40",
+    "url": "/atube/index.html"
+  },
+  {
+    "revision": "eae75f943c96ef76435c",
+    "url": "/atube/static/css/main.f58c078f.chunk.css"
+  },
+  {
+    "revision": "f21c00473f48e12a8dda",
+    "url": "/atube/static/js/2.bcfdd4b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/atube/static/js/2.bcfdd4b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eae75f943c96ef76435c",
+    "url": "/atube/static/js/main.b0f6c02b.chunk.js"
+  },
+  {
+    "revision": "db3a9f7dd83ccda5c806",
+    "url": "/atube/static/js/runtime-main.3eb2a102.js"
+  },
+  {
+    "revision": "713767b569928de507e72ca787d02870",
+    "url": "/atube/static/media/gif.713767b5.gif"
+  }
+]);
